d37347bf1e2de46cd5740f43ea5c44c2afc6c8fa
omranh88-ship-it <omran.h88@gmail.com>
2025-12-29 22:58:03 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "workout-cache-v1";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
