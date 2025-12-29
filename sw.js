9424f19b0daa9d9faf555b28912b68535b31e3e6
omranh88-ship-it <omran.h88@gmail.com>
2025-12-30 00:08:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "workout-cache-v1";
+const CACHE = "workout-cache-v3";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
